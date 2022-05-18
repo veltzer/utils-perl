d918dadc2292487b52bc059e03829aea985407b2
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:15 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "utils-perl"
-project_long_description = "Various utilities written in perl"
-project_year_started = 2016
-project_description = """Some utilities I wrote in perl"""
+description_short = "Various utilities written in perl"
